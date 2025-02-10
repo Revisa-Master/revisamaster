dac83dc95b8500427580be8d032ac445317e6ab4
Leonardo de Sa <leonardocostasa95@gmail.com>
2025-02-10 12:01:41 -0300

    feat: adicionada animação ao botão do WhatsApp com @keyframes
    - Criada animação para o botão fixo do WhatsApp utilizando @keyframes.
    - Implementada animação em todas as telas do projeto para melhor interação visual.
    - Ajustes de transição e responsividade para garantir um efeito suave e agradável.

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -5,9 +5,9 @@ const myObserver = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
-        else{
+        /*else{
             entry.target.classList.remove('show');
-        }
+        }*/
     });
 });
 
