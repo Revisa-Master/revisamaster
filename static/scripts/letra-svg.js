5cc69c900fd1c1be065f0d93b7d0f9e2d34b3de2
Leonardo de Sa <leonardocostasa95@gmail.com>
2025-02-10 15:18:09 -0300

    feat: adicionadas animações na página services.html
    - Criado script para manipulação de animações e interatividade.
    - Implementadas animações utilizando `@keyframes` para transições suaves.
    - Aplicadas animações nos elementos da página `services.html` para melhorar a experiência do usuário.

--- static/scripts/letra-svg.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const svg = document.querySelector('svg');
-    const textElements = svg.querySelectorAll('text');
-    textElements.forEach(el => {
-      el.style.fontFamily = "'Questrial', sans-serif";
-    });
-  });
---
+++ static/scripts/scripts-services.js
@@ -0,0 +1,71 @@
+/*=============================================
+    CRIANDO CALLBACKS PARA ANIMAÇÕES
+===============================================*/
+
+const leftObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('left__to__right');
+            leftObserver.unobserve(entry.target); // Stop observing once animated
+        }
+    });
+});
+
+const rightObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('right__to__left');
+            rightObserver.unobserve(entry.target); // Stop observing once animated
+        }
+    });
+});
+
+const fadeIn = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade__in');
+            rightObserver.unobserve(entry.target); // Stop observing once animated
+        }
+    });
+});
+
+/*=========================================================
+    CAPTURANDO SELETORES NO HTML HERO SECTION E SECTION 2
+============================================================ */
+
+document.querySelectorAll('.section-content-2__container, .hero-section__hero-text')
+    .forEach(element => {
+        fadeIn.observe(element);
+    });
+
+/*=============================================
+    CAPTURANDO SELETORES NO HTML SECTION 3
+===============================================*/
+
+document.querySelectorAll('.section-content-3__container__service-block-left-1')
+    .forEach(element => {
+        leftObserver.observe(element);
+    });
+
+document.querySelectorAll('.section-content-3__container__service-block-left-2')
+    .forEach(element => {
+        leftObserver.observe(element);
+    });
+
+
+document.querySelectorAll('.section-content-3__container__service-block-left-3')
+    .forEach(element => {
+        leftObserver.observe(element);
+    });    
+
+//------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------//
+
+document.querySelectorAll('.section-content-3__container__service-block-right-1')
+    .forEach(element => rightObserver.observe(element));
+
+document.querySelectorAll('.section-content-3__container__service-block-right-2')
+    .forEach(element => rightObserver.observe(element));
+
+document.querySelectorAll('.section-content-3__container__service-block-right-3')
+    .forEach(element => rightObserver.observe(element));
